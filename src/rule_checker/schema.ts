af003f747c69297ae56a2e23206ef967b45483a5
Thomas Kercheval <spacekattpoispin@gmail.com>
2019-05-10 16:02:52 -0700

    Add rule_checker schema stub

---
+++ src/rule_checker/schema.ts
@@ -0,0 +1,6 @@
+//import * from AJV form 'ajv';
+//import {} from './interfaces';
+
+// TODO: define RuleConfig interface
+// TODO: create schema with `typescript-json-schema-tool`
+// TODO: create validation functions with AJV
