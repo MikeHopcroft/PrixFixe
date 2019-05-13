2b3330c860b670b51e0f674dd75d8a9860b1e774
Thomas Kercheval <spacekattpoispin@gmail.com>
2019-05-13 15:53:08 -0700

    Add validator for RuleConfig schema

--- src/rule_checker/schema.ts
+++ src/rule_checker/schema.ts
@@ -1,6 +1,76 @@
-//import * from AJV form 'ajv';
-//import {} from './interfaces';
+import * as AJV from 'ajv';
 
-// TODO: define RuleConfig interface
-// TODO: create schema with `typescript-json-schema-tool`
-// TODO: create validation functions with AJV
+import {
+    RuleConfig,
+} from './interfaces';
+
+const ruleConfigSchema = {
+    "$schema": "http://json-schema.org/draft-07/schema#",
+    "definitions": {
+        "PartialRule": {
+            "properties": {
+                "exclusionZones": {
+                    "items": {
+                        "additionalProperties": false,
+                        "patternProperties": {
+                            "^[0-9]+$": {
+                                "items": {
+                                    "type": "number",
+                                },
+                                "type": "array",
+                            },
+                        },
+                        "type": "object",
+                    },
+                    "type": "array",
+                },
+                "partialKey": {
+                    "type": "string",
+                },
+                "validCatagoryMap": {
+                    "additionalProperties": false,
+                    "patternProperties": {
+                        "^[0-9]+$": {
+                            "items": {
+                                "type": "number",
+                            },
+                            "type": "array",
+                        },
+                    },
+                    "type": "object",
+                },
+            },
+            "required": [
+                "exclusionZones",
+                "partialKey",
+                "validCatagoryMap",
+            ],
+            "type": "object",
+        },
+    },
+    "properties": {
+        "rules": {
+            "items": {
+                "$ref": "#/definitions/PartialRule",
+            },
+            "type": "array",
+        },
+    },
+    "required": [
+        "rules",
+    ],
+    "type": "object",
+};
+
+const ajv = new AJV();
+const ruleConfigValidator = ajv.compile(ruleConfigSchema);
+
+export const validateRuleConfig = (ruleConfig: RuleConfig) => {
+    if (!ruleConfigValidator(ruleConfig)) {
+        const message =
+            'validateRuleConfig: Invalid `rules.yaml` config file.';
+        console.error(message);
+        console.error(ruleConfigValidator.errors);
+        throw TypeError(message);
+    }
+};
