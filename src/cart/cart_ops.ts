4a576bdc61509dbfa5cff70df87bf504f5ae6c96
Mike Hopcroft <mhop@microsoft.com>
2019-05-31 13:59:54 -0700

    ICartOps refactor complete

--- src/cart/cart_ops.ts
+++ src/cart/cart_ops.ts
@@ -1,133 +1,9 @@
 import { AID, AttributeInfo, MatrixEntityBuilder } from '../attributes';
 import { Catalog, KEY, PID } from '../catalog';
 
-import { Cart, ItemInstance } from './interfaces';
+import { Cart, FindItemPredicate, ICartOps, ItemInstance } from './interfaces';
 import { IDGenerator } from '../unified';
 import { RuleChecker } from '../rule_checker';
-
-type FindItemPredicate = (item: ItemInstance) => boolean;
-
-// NOTE: disabling tslint rule locally because TSLint only offers the choice of
-// all interfaces start with 'I' or no interfaces start with 'I'. On this
-// project, we use the 'I' prefix for interfaces that are like abstract base
-// classes, but not interfaces that are POJO structs.
-// tslint:disable-next-line:interface-name
-export interface ICartOps {
-    ///////////////////////////////////////////////////////////////////////////
-    //
-    // Adding ItemInstances
-    //
-    ///////////////////////////////////////////////////////////////////////////
-    // Returns a shallow copy of the Cart, with the ItemInstance appended.
-    addToCart(cart: Cart, item: ItemInstance): Cart;
-
-    // Returns a shallow copy of the ItemInstance with the OptionInstance
-    // appended. Does not verify that the option is legal for the item.
-    addToItem(parent: ItemInstance, child: ItemInstance): ItemInstance;
-
-    ///////////////////////////////////////////////////////////////////////////
-    //
-    // Finding ItemInstances
-    //
-    ///////////////////////////////////////////////////////////////////////////
-    // Returns an iterable of ItemInstances in the cart with a particular KEY.
-    // Items are returned in the order they were added to the cart.
-    //
-    // Use case: find all instances of a specific drink like a 'large iced latte'.
-    //
-    findByKey(cart: Cart, key: KEY): IterableIterator<ItemInstance>;
-
-    // Returns an iterable of ItemInstances in the cart that correspond to a
-    // particular PID (instance of generic entity). Items are returned in the
-    // order they were added to the cart.
-    //
-    // Use case: want to find all lattes, regardless of attributes. Searching with
-    // the PID for 'latte' might return ItemInstances for a 'medium iced latte'
-    // and a 'small hot latte'.
-    //
-    findByPID(cart: Cart, pid: PID): IterableIterator<ItemInstance>;
-
-    // Returns an iterable of ItemInstances that contain an child with a
-    // particular KEY (instance of specific entity). Items are returned in the
-    // order they were added to the cart.
-    findByChildKey(cart: Cart, key: KEY): IterableIterator<ItemInstance>;
-
-    // Returns an iterable of ItemInstances in the cart that correspond to a
-    // particular PID (instance of generic entity). Items are returned in the
-    // order they were added to the cart.
-    //
-    // Use case: want to find all children that are lattes, regardless of their
-    // attributes. Searching with the PID for 'latte' might return child
-    // ItemInstances for a 'medium iced latte' and a 'small hot latte'.
-    //
-    findByChildPID(cart: Cart, pid: PID): IterableIterator<ItemInstance>;
-
-    // Returns an iterable of parent ItemInstances that can legally hold a
-    // child whose type is specified by `childKey`. Items are returned in
-    // the order they were added to the cart.
-    findCompatibleParent(
-        cart: Cart,
-        childKey: KEY
-    ): IterableIterator<ItemInstance>;
-
-    // Returns an iterable of ItemInstances in the Cart for which a
-    // specified predicate is true. Items are returned in the order they
-    // were added to the cart.
-    findInCart(
-        cart: Cart,
-        predicate: FindItemPredicate
-    ): IterableIterator<ItemInstance>;
-
-    // Returns an iterable of ItemInstances in an array for which a specified
-    // predicate is true.
-    findInItemArray(
-        items: ItemInstance[],
-        predicate: FindItemPredicate
-    ): IterableIterator<ItemInstance>;
-
-    ///////////////////////////////////////////////////////////////////////////
-    //
-    // Replacing ItemInstances
-    //
-    ///////////////////////////////////////////////////////////////////////////
-    // Returns a shallow copy of the Cart, where the item that shares the new
-    // item's UID is replaced with the new item.
-    replaceInCart(cart: Cart, item: ItemInstance): Cart;
-
-    ///////////////////////////////////////////////////////////////////////////
-    //
-    // Removing ItemInstances
-    //
-    ///////////////////////////////////////////////////////////////////////////
-    // Returns a shallow copy of the cart, omitting the item with the same
-    // UID as the specified item.
-    removeFromCart(cart: Cart, item: ItemInstance): Cart;
-
-    ///////////////////////////////////////////////////////////////////////////
-    //
-    // Operations relating to ItemInstances
-    //
-    ///////////////////////////////////////////////////////////////////////////
-    // Creates an ItemInstance whose key is determined by the pid and aids.
-    // This method generates a unique value for the ItemInstance's uid field.
-    createItem(
-        quantity: number,
-        pid: PID,
-        aids: IterableIterator<AID>,
-        children: IterableIterator<ItemInstance>
-    ): ItemInstance;
-
-    // Returns a new ItemInstance whose key is derived from the original
-    // item's attributes, with some being replaced by values in newAIDs.
-    changeItemAttributes(
-        item: ItemInstance,
-        newAIDs: IterableIterator<AID>
-    ): ItemInstance;
-
-    // Returns a new ItemInstance whose key is derived from the original
-    // item's attributes, with a different PID.
-    changeItemPID(item: ItemInstance, newPID: PID): ItemInstance;
-}
 
 export class CartOps implements ICartOps {
     attributeInfo: AttributeInfo;
@@ -170,7 +46,8 @@ export class CartOps implements ICartOps {
     // Returns an iterable of ItemInstances in the cart with a particular KEY.
     // Items are returned in the order they were added to the cart.
     //
-    // Use case: find all instances of a specific drink like a 'large iced latte'.
+    // Use case: find all instances of a specific drink like a 'large iced
+    // latte'.
     //
     *findByKey(cart: Cart, key: KEY): IterableIterator<ItemInstance> {
         const predicate = (item: ItemInstance) => key === item.key;
@@ -181,9 +58,9 @@ export class CartOps implements ICartOps {
     // particular PID (instance of generic entity). Items are returned in the
     // order they were added to the cart.
     //
-    // Use case: want to find all lattes, regardless of attributes. Searching with
-    // the PID for 'latte' might return ItemInstances for a 'medium iced latte'
-    // and a 'small hot latte'.
+    // Use case: want to find all lattes, regardless of attributes. Searching
+    // with the PID for 'latte' might return ItemInstances for a 'medium iced
+    // latte' and a 'small hot latte'.
     //
     *findByPID(cart: Cart, pid: PID): IterableIterator<ItemInstance> {
         const predicate = (item: ItemInstance) => {
@@ -282,10 +159,12 @@ export class CartOps implements ICartOps {
         const modified: ItemInstance[] = [];
         for (const existing of items) {
             if (changed) {
-                // If we've already swapped in the replacement, just copy existing.
+                // If we've already swapped in the replacement, just copy
+                // existing.
                 modified.push(existing);
             } else {
-                // Otherwise, search the existing item for the replacement target.
+                // Otherwise, search the existing item for the replacement
+                // target.
                 if (replacement.uid === existing.uid) {
                     // Replace existing item.
                     modified.push(replacement);
@@ -336,10 +215,12 @@ export class CartOps implements ICartOps {
         const modified: ItemInstance[] = [];
         for (const existing of items) {
             if (changed) {
-                // If we've already swapped in the replacement, just copy existing.
+                // If we've already swapped in the replacement, just copy
+                // existing.
                 modified.push(existing);
             } else {
-                // Otherwise, search the existing item for the replacement target.
+                // Otherwise, search the existing item for the replacement
+                // target.
                 if (remove.uid === existing.uid) {
                     // Don't copy the removed item.
                     changed = true;
@@ -404,7 +285,8 @@ export class CartOps implements ICartOps {
             builder.addAttribute(aid);
         }
 
-        // Copy over new attributes, overwriting existing attributes on same dimension.
+        // Copy over new attributes, overwriting existing attributes on same
+        // dimension.
         for (const aid of newAIDs) {
             builder.setAttribute(aid);
         }
--- src/cart/interfaces.ts
+++ src/cart/interfaces.ts
@@ -51,97 +51,132 @@ export interface Cart {
 //
 ///////////////////////////////////////////////////////////////////////////////
 
+export type FindItemPredicate = (item: ItemInstance) => boolean;
+
 // NOTE: disabling tslint rule locally because TSLint only offers the choice of
 // all interfaces start with 'I' or no interfaces start with 'I'. On this
 // project, we use the 'I' prefix for interfaces that are like abstract base
 // classes, but not interfaces that are POJO structs.
 // tslint:disable-next-line:interface-name
-export interface ICartOpsOld {
+// NOTE: disabling tslint rule locally because TSLint only offers the choice of
+// all interfaces start with 'I' or no interfaces start with 'I'. On this
+// project, we use the 'I' prefix for interfaces that are like abstract base
+// classes, but not interfaces that are POJO structs.
+// tslint:disable-next-line:interface-name
+export interface ICartOps {
+    ///////////////////////////////////////////////////////////////////////////
     //
-    // Operations involving Cart.
+    // Adding ItemInstances
     //
-
-    // Returns a list of ItemInstances in the cart with a particular SPID.
-    // Items are returned in the order they were added to the cart.
-    //
-    // Use case: find all instances of a specific drink like a 'large iced latte'.
-    //
-    findItemByKey(cart: Cart, key: KEY): IterableIterator<ItemInstance>;
-
-    // Returns a list of ItemInstances in the cart that correspond to a
-    // particular GPID. Items are returned in the order they were added to the
-    // cart.
-    //
-    // Use case: want to find all lattes, regardless of attributes. Searching with
-    // the GPID for 'latte' might return ItemInstances for a 'medium iced latte'
-    // and a 'small hot latte'.
-    //
-    findItemByPID(cart: Cart, pid: PID): IterableIterator<ItemInstance>;
-
-    // Returns a list of ItemInstances that contain an OptionInstance with a
-    // particular SPID. Items are returned in the order they were added to the
-    // cart.
-    //
-    // ISSUE: do we want a corresponding version that finds options associated
-    // with a certain generic option.
-    findItemByChildKey(cart: Cart, key: KEY): IterableIterator<ItemInstance>;
-    findItemByChildPID(cart: Cart, PID: PID): IterableIterator<ItemInstance>;
-
-    // Returns a list of ItemInstances that are allowed to contain an
-    // OptionInstance with a particular SPID. Items are returned in the order
-    // they were added to the cart.
-    //
-    // ISSUSE: does this return ItemInstances where the OptionInstance can be
-    // successfully added (e.g. not violating quantity, mutual exclusivity
-    // constraints) or return ItemInstances whose SPIDs allow this type of
-    // OptionInstance.
-    // ISSUE: TODO: should we pass RuleChecker or get it from member variable.
-    // Depends if we make a class or not.
-    findCompatibleItems(
-        cart: Cart,
-        option: ItemInstance
-    ): IterableIterator<ItemInstance>;
-
-    //
-    // Operations involving ItemInstances
-    //
-
-    findChildByKey(
-        item: ItemInstance,
-        key: KEY
-    ): IterableIterator<ItemInstance>;
-    findChildByPID(
-        item: ItemInstance,
-        pid: PID
-    ): IterableIterator<ItemInstance>;
-
+    ///////////////////////////////////////////////////////////////////////////
     // Returns a shallow copy of the Cart, with the ItemInstance appended.
-    addItem: (cart: Cart, item: ItemInstance) => Cart;
-
-    // Returns a shallow copy of the Cart, where the item that shares the new
-    // item's UID is replaced with the new item.
-    replaceItem: (cart: Cart, item: ItemInstance) => Cart;
-
-    // Returns a shallow copy of the cart, omitting the item with the specific
-    // UID.
-    // TODO: ISSUE: throw or silently return when item not in cart.
-    removeItem: (cart: Cart, item: ItemInstance) => Cart;
-
-    //
-    // Operations involving OptionInstances
-    //
+    addToCart(cart: Cart, item: ItemInstance): Cart;
 
     // Returns a shallow copy of the ItemInstance with the OptionInstance
     // appended. Does not verify that the option is legal for the item.
-    addChild(parent: ItemInstance, child: ItemInstance): ItemInstance;
+    addToItem(parent: ItemInstance, child: ItemInstance): ItemInstance;
 
-    // Returns a shallow copy of the ItemInstance, where the option that
-    // shares the new option's UID is replaced with the new option.
-    updateChild(parent: ItemInstance, child: ItemInstance): ItemInstance;
+    ///////////////////////////////////////////////////////////////////////////
+    //
+    // Finding ItemInstances
+    //
+    ///////////////////////////////////////////////////////////////////////////
+    // Returns an iterable of ItemInstances in the cart with a particular KEY.
+    // Items are returned in the order they were added to the cart.
+    //
+    // Use case: find all instances of a specific drink like a 'large iced
+    // latte'.
+    //
+    findByKey(cart: Cart, key: KEY): IterableIterator<ItemInstance>;
 
-    // Returns a shallow copy of the ItemInstance, omitting the option with
-    // the specific UID.
-    removeChild(parent: ItemInstance, child: ItemInstance): ItemInstance;
+    // Returns an iterable of ItemInstances in the cart that correspond to a
+    // particular PID (instance of generic entity). Items are returned in the
+    // order they were added to the cart.
+    //
+    // Use case: want to find all lattes, regardless of attributes. Searching
+    // with the PID for 'latte' might return ItemInstances for a 'medium iced
+    // latte' and a 'small hot latte'.
+    //
+    findByPID(cart: Cart, pid: PID): IterableIterator<ItemInstance>;
 
-    updateAttributes(parent: ItemInstance, attributes: Set<AID>): ItemInstance;
+    // Returns an iterable of ItemInstances that contain an child with a
+    // particular KEY (instance of specific entity). Items are returned in the
+    // order they were added to the cart.
+    findByChildKey(cart: Cart, key: KEY): IterableIterator<ItemInstance>;
+
+    // Returns an iterable of ItemInstances in the cart that correspond to a
+    // particular PID (instance of generic entity). Items are returned in the
+    // order they were added to the cart.
+    //
+    // Use case: want to find all children that are lattes, regardless of their
+    // attributes. Searching with the PID for 'latte' might return child
+    // ItemInstances for a 'medium iced latte' and a 'small hot latte'.
+    //
+    findByChildPID(cart: Cart, pid: PID): IterableIterator<ItemInstance>;
+
+    // Returns an iterable of parent ItemInstances that can legally hold a child
+    // whose type is specified by `childKey`. Items are returned in the order
+    // they were added to the cart.
+    findCompatibleParent(
+        cart: Cart,
+        childKey: KEY
+    ): IterableIterator<ItemInstance>;
+
+    // Returns an iterable of ItemInstances in the Cart for which a specified
+    // predicate is true. Items are returned in the order they were added to the
+    // cart.
+    findInCart(
+        cart: Cart,
+        predicate: FindItemPredicate
+    ): IterableIterator<ItemInstance>;
+
+    // Returns an iterable of ItemInstances in an array for which a specified
+    // predicate is true.
+    findInItemArray(
+        items: ItemInstance[],
+        predicate: FindItemPredicate
+    ): IterableIterator<ItemInstance>;
+
+    ///////////////////////////////////////////////////////////////////////////
+    //
+    // Replacing ItemInstances
+    //
+    ///////////////////////////////////////////////////////////////////////////
+    // Returns a shallow copy of the Cart, where the item that shares the new
+    // item's UID is replaced with the new item.
+    replaceInCart(cart: Cart, item: ItemInstance): Cart;
+
+    ///////////////////////////////////////////////////////////////////////////
+    //
+    // Removing ItemInstances
+    //
+    ///////////////////////////////////////////////////////////////////////////
+    // Returns a shallow copy of the cart, omitting the item with the same UID
+    // as the specified item.
+    removeFromCart(cart: Cart, item: ItemInstance): Cart;
+
+    ///////////////////////////////////////////////////////////////////////////
+    //
+    // Operations relating to ItemInstances
+    //
+    ///////////////////////////////////////////////////////////////////////////
+    // Creates an ItemInstance whose key is determined by the pid and aids. This
+    // method generates a unique value for the ItemInstance's uid field.
+    createItem(
+        quantity: number,
+        pid: PID,
+        aids: IterableIterator<AID>,
+        children: IterableIterator<ItemInstance>
+    ): ItemInstance;
+
+    // Returns a new ItemInstance whose key is derived from the original item's
+    // attributes, with some being replaced by values in newAIDs.
+    changeItemAttributes(
+        item: ItemInstance,
+        newAIDs: IterableIterator<AID>
+    ): ItemInstance;
+
+    // Returns a new ItemInstance whose key is derived from the original item's
+    // attributes, with a different PID.
+    changeItemPID(item: ItemInstance, newPID: PID): ItemInstance;
 }
