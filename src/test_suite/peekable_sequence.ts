764c45b64b181d3d25e3a8d977ff43bbf0553f2f
Michael Hopcroft <mhop@microsoft.com>
2019-08-16 10:28:29 -0700

    WIP: parser for suite filter expressions

---
+++ src/test_suite/peekable_sequence.ts
@@ -0,0 +1,47 @@
+export class PeekableSequence<T> {
+    iterator: IterableIterator<T>;
+    current: IteratorResult<T>;
+
+    constructor(iterator: IterableIterator<T>) {
+        this.iterator = iterator;
+        this.current = this.iterator.next();
+    }
+
+    peek(): T {
+        if (!this.current.done) {
+            return this.current.value;
+        } else {
+            throw TypeError('PeekableSequence<T>.peek(): at end of stream.');
+        }
+    }
+
+    get(): T {
+        if (!this.current.done) {
+            const value = this.current.value;
+            this.current = this.iterator.next();
+            return value;
+        } else {
+            throw TypeError('PeekableSequence<T>.get(): at end of stream.');
+        }
+    }
+
+    atEOS(): boolean {
+        return this.current.done;
+    }
+
+    skip(value: T): boolean {
+        if (!this.atEOS() && this.peek() === value) {
+            this.get();
+            return true;
+        }
+        return false;
+    }
+
+    nextIs(value: T): boolean {
+        if (!this.atEOS()) {
+            return this.peek() === value;
+        }
+
+        return false;
+    }
+}
