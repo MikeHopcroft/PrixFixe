d528e2fc32fea94dc39f40924b91bb5a567df282
Michael Hopcroft <mhop@microsoft.com>
2020-06-19 15:02:19 -0700

    WIP: loadCatalogSpec and ILoader

--- src/core/authoring/loader.ts
+++ src/core/authoring/loader.ts
@@ -15,7 +15,98 @@ import {
   RecipeSpec,
 } from './types';
 
-export function loadCatalog(filename: string): CatalogSpec {
+// tslint:disable-next-line:interface-name
+export interface ILoader {
+  push(name: string): Promise<string>;
+  // push(name: string): void;
+  pop(): void;
+}
+
+export class FileLoader implements ILoader {
+  private readonly context: string[];
+
+  constructor(root?: string) {
+    this.context = [root || process.cwd()];
+  }
+
+  push(name: string): Promise<string> {
+    // Resolve filename relative to previous file.
+    const resolved: string = path.resolve(
+      this.context[this.context.length - 1],
+      name
+    );
+
+    // NOTE: context is not popped when readFile() fails.
+    this.context.push(path.dirname(resolved));
+
+    console.log(`readFile(${resolved})`);
+
+    return new Promise<string>((resolve, reject) => {
+      fs.readFile(resolved, 'utf8', (err, data) => {
+        console.log(`readFile returned: ${err}`);
+        err ? reject(err) : resolve(data);
+      });
+    });
+  }
+
+  pop(): void {
+    this.context.pop();
+  }
+}
+
+export async function loadCatalogSpec(
+  loader: ILoader,
+  name: string
+): Promise<CatalogSpec> {
+  const dimensions: DimensionSpec[] = [];
+  const tensors: TensorSpec[] = [];
+  const catalog: GroupSpec[] = [];
+  const rules: AnyRule[] = [];
+  const recipes: RecipeSpec[] = [];
+
+  await process(name);
+  return { dimensions, tensors, catalog, rules, recipes };
+
+  async function process(fileName: string) {
+    console.log(`Processing ${fileName}`);
+    const text = await loader.push(fileName);
+    // console.log(`text="${text}"`);
+    const root = yaml.safeLoad(text);
+    const catalog = validate(partialCatalogSpecType, root);
+
+    merge(catalog);
+
+    if (catalog.imports !== undefined) {
+      for (const nestedFileName of catalog.imports) {
+        await process(nestedFileName);
+      }
+    }
+
+    // TODO: should this be in a catch block?
+    loader.pop();
+  }
+
+  function merge(c: PartialCatalogSpec) {
+    // console.log(`merge ${JSON.stringify(c, null, 2)}`);
+    if (c.dimensions) {
+      dimensions.push(...c.dimensions);
+    }
+    if (c.tensors) {
+      tensors.push(...c.tensors);
+    }
+    if (c.catalog) {
+      catalog.push(...c.catalog);
+    }
+    if (c.rules) {
+      rules.push(...c.rules);
+    }
+    if (c.recipes) {
+      recipes.push(...c.recipes);
+    }
+  }
+}
+
+export function loadCatalogFile(filename: string): CatalogSpec {
   const builder = new CatalogBuilder();
   builder.process(filename);
   return builder.build();
